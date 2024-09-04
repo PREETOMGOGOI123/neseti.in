0f36ed277b6293611a85ea98b1a6238bc85920d8
PREETOMGOGOI123 <preetomgogoi>
2024-09-05 05:21:23 +0530

    mobile screen changes

--- src/lib/layout/Header/Header.svelte
+++ src/lib/layout/Header/Header.svelte
@@ -1,10 +1,29 @@
 <script>
   import logo from "$lib/images/neseti-logo.png";
   import NavMenu from "./NavBar/NavMenu.svelte";
+  import { onMount } from 'svelte';
+
+  let isMenuOpen = false;
+  let isMobile = false;
+  let isTablet = false;
+
+  function toggleMenu() {
+    isMenuOpen = !isMenuOpen;
+  }
+
+  onMount(() => {
+    const checkScreenSize = () => {
+      isMobile = window.innerWidth < 768;
+      isTablet = window.innerWidth >= 768 && window.innerWidth < 1024;
+    };
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+    return () => window.removeEventListener('resize', checkScreenSize);
+  });
 </script>
 
 <header class="bg-white shadow-md">
-  <nav class="container mx-auto px-6 py-4 flex items-center justify-between">
+  <nav class="container mx-auto px-6 py-4 flex items-center justify-between relative">
     <div class="flex items-center">
       <img src={logo} alt="Logo" class="h-12 w-auto mr-6" />
       <a
@@ -13,5 +32,19 @@
         >Neseti</a
       >
     </div>
-    <NavMenu />
+    {#if isMobile || isTablet}
+      <button on:click={toggleMenu} class="text-gray-600 hover:text-gray-900 focus:outline-none z-[10001] relative">
+        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </button>
+    {:else}
+      <NavMenu />
+    {/if}
+  </nav>
+  {#if (isMobile || isTablet) && isMenuOpen}
+    <div class="px-6 py-4 bg-white">
+      <NavMenu />
+    </div>
+  {/if}
 </header>
--- src/lib/layout/Header/NavBar/NavMenu.svelte
+++ src/lib/layout/Header/NavBar/NavMenu.svelte
@@ -17,13 +17,13 @@
 	}
 </script>
 
-<ul class="flex space-x-6">
+<ul class="fixed top-0 right-0 flex flex-col w-3/5 h-full bg-white  shadow-lg p-6 space-y-4 overflow-y-auto md:static md:flex-row md:w-auto md:h-auto md:bg-transparent md:shadow-none md:p-0 md: pt-24 md:space-y-0 md:space-x-6 md:overflow-visible md:justify-end z-[9999]">
 	{#each menuItems as item}
-		<li>
+		<li class="z-[9999]">
 			<a
 				href={`${base}${item.href}`}
 				on:click={(event) => handleClick(event, item.href)}
-				class="text-gray-700 hover:text-blue-600 transition duration-300"
+				class="block text-gray-700 hover:text-blue-600 transition duration-300 py-2 md:py-0 text-right md:text-left z-[9999]"
 			>
 				{item.label}
 			</a>
