72e76f6a191535d5a752a490ada79c07dffba0e9
PREETOMGOGOI123 <preetomgogoi>
2024-09-18 11:07:58 +0530

    changed the base path

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
     
     // Path configuration for GitHub Pages
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+      base: process.argv.includes('dev') ? '' : '/neseti',
     },
   
 
